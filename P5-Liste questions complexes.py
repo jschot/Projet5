55208f9184e06623de15ba1b2264aa9138a30e5b
GuillaumeEyovuaht <44196438+GuillaumeEyovuaht@users.noreply.github.com>
2019-04-13 15:37:00 +0200

    Add files via upload

---
+++ P5-Liste questions complexes.py
@@ -0,0 +1,12 @@
+# Liste question complexe
+
+question1 = [("La Première Croisade a commencé en 1096."),True,("")]
+question2 = [("Le bombardement de Pearl Harbor a été effectué le 7 décembre 1940."),False,("C'est faux, c'était le 7 décembre 1941")]
+question3 = [("Une tête humaine a en moyenne 110 000 follicules pileux."),True,("")]
+question4 = [("Un bébé a moins d'os qu'un adulte."),False,("C'est faux, c'est même le contraire, il en a plus et plusieurs os se souderont ensemble durant la croissance.")]
+question5 = [("Cléopâtre VII était égyptienne d'origine."),False,("C'est faux, elle était d'origine grecque.")]
+question6 = [("Un café décaféiné ne contient pas de caféine."),False,("C'est faux, il en contient mais beaucoup moins.")]
+question7 = ["Les Fidji sont un archipel de plus de 300 îles situé en Océanie."),True,("")]
+question8 = [("Un kangourou roux peut parcourir 9 mètres en un bond."),True,("")]
+question9 = [("Un koala boit jusqu'à 4 litres d'eau par jour."),False,("C'est faux, il ne boit pas, le liquide provenant des feuilles d'eucalyptus lui suffit.")]
+question10 = [("Le Japon est un archipel constitué de 6 852 îles."),True,("")]
